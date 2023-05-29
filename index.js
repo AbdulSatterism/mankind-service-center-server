7a7e3975bfeb992b2b25412cb6296e220607eda9
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2023-05-29 21:49:17 +0600

    add crud operation for MSC

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+//middle wares
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hlsud.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const serviceCollection = client.db('mankindServiceCenter').collection('services');
+        const orderCollection = client.db('mankindServiceCenter').collection('orders');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
+
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result)
+        })
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+
+        });
+
+        //get orders by email 
+        app.get('/orders', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = { email: req.query.email }
+            };
+            const cursor = orderCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        });
+
+        //delete orders
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+};
+
+run().catch(error => console.log(error))
+
+
+
+app.get('/', (req, res) => {
+    res.send('Mankind Service Center server is running');
+});
+
+app.listen(port, () => {
+    console.log(`port is running on ${port}`)
+})
